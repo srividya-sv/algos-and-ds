58e9be261a187d03b8a11a5f92f1db6834453ce5
srividya-sv <50850030+srividya-sv@users.noreply.github.com>
2019-06-13 16:53:14 -0700

    Create 158-readNCharacters.java

---
+++ 158-readNCharacters.java
@@ -0,0 +1,33 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf); 
+ */
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    char[] prevBuf = new char[4];
+    int prevSize = 0;
+    int prevIndex = 0;
+    
+    public int read(char[] buf, int n) {
+        int counter = 0;
+        
+        while (counter < n) {
+            if (prevIndex < prevSize) {
+                buf[counter++] = prevBuf[prevIndex++];
+            } else {
+                prevSize = read4(prevBuf);
+                prevIndex = 0;
+                if (prevSize == 0) {
+                    // no more data to consume from stream
+                    break;
+                }
+            }
+        }
+        return counter;
+    }
+    
+}
